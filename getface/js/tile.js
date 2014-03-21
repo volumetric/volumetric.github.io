4ab627539c6b0d8c17d4f2d082042b48d2149277
Vinit <vinit.iitk@gmail.com>
2014-03-21 18:24:55 +0530

    local images

--- getface/js/tile.js
+++ getface/js/tile.js
@@ -3,18 +3,18 @@ function Tile(position, value) {
   this.y                = position.y;  
   this.value            = value || 2;
 
-  var img_url = [
-  'http://images.iimg.in/i/50830c6aedaffd18ab000135-100-100-1355921371/priyanka-chopra.img',
-  'http://www.centralchronicle.com/wp-content/uploads/2012/09/Bipasha-basu1-100x100.jpg',
-  'http://static4.wikia.nocookie.net/__cb62501/swrpg/images/thumb/6/6b/Salma-Hayek-salma-hayek-248829_1024.jpg/100px-2,390,0,387-Salma-Hayek-salma-hayek-248829_1024.jpg',
-  'http://www.inquisitr.com/wp-content/uploads/2014/03/angelina-jolie-2655-100x100.jpg',
-  'http://images6.fanpop.com/image/photos/32700000/Drew-drew-barrymore-32760050-100-100.png',
-  'http://www.lahiguera.net/cinemania/actores/penelope_cruz/fotos/7386/penelope_cruz-p.jpg',
-  'http://fanoos.com/ia/kareena_kapoors.jpg',
-  'http://img.india-forums.com/images/100x100/138211-malaika-arora-khan-receives-jeeyo-bollywood-awards.jpg',
-  'http://img.bollycurry.com/images/100x100/28072-amisha-patel.jpg',
-  'http://www.ebharat.in/sites/default/files/imagecache/node-gallery-thumbnail/Chitrangada%20Singh.jpeg',
-  'http://img.india-forums.com/images/100x100/293541-deepika-padukone.jpg',
+var img_url = [
+  'img/priyanka_chopra.jpg',
+  'img/bipasha_basu.jpg',
+  'img/salma_hayek.jpg',
+  'img/angelina_jolie.jpg',
+  'img/drew_barrymore.jpg',
+  'img/penelope_cruz.jpg',
+  'img/kareena_kapoor.jpg',
+  'img/malaika_arora.jpg',
+  'img/amisha_patel.jpg',
+  'img/chitrangada_singh.jpg',
+  'img/deepika_padukone.jpg',
   ];
 
   var power = Math.round(Math.log(this.value)/Math.log(2)) - 1;
